06cb8b5ff1d07164f8ef87b23feda8feb5bf3672
Eduard Fossas <eduardfp@gmail.com>
2024-03-30 15:54:05 +0100

    try

--- pages/letter-modern/index.js
+++ pages/letter-modern/index.js
@@ -295,7 +295,7 @@ export default function Home({}) {
             <article className="text-2xl relative md:text-3xl 2xl:text-4xl">
               <div className="grid grid-cols-12 md:gap-10 mb-10">
                 <figure className="col-span-full md:col-start-3 md:col-span-8 md:mb-10">
-                  <View className="relative h-80 w-full aspect-square md:aspect-video">
+                  <View className="w-full h-64 md:h-96">
                     <SphereVisual />
                   </View>
 
