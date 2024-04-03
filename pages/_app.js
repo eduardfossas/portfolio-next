f8426dde2c4bce02536b0e21424cbc2a0fcfd058
Eduard Fossas <eduardfp@gmail.com>
2024-04-03 17:41:56 +0200

    Add analy

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import { SmoothScroll } from "components/SmoothScroll";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <SmoothScroll>
       <Component key={router.asPath} {...pageProps} />
+      <Analytics />
     </SmoothScroll>
   );
 }
